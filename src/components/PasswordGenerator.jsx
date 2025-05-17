d115328586f594fe9725873207dff9f671e9f6f9
Arek <arek93r@gmail.com>
2025-05-17 10:59:57 +0200

    add state with useState for all options

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PasswordGenerator() {
+  // przechowuje długość hasła
+  const [length, setLength] = useState(12);
+
+  // czy mają być znaki specjalne
+  const [includeSymbols, setIncludeSymbols] = useState(true);
+
+  // czy mają być cyfry
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+
+  // czy mają być wielkie litery
+  const [includeUppercase, setIncludeUppercase] = useState(true);
+
   return (
     <div>
       <h2>Opcje Generatora</h2>
@@ -8,26 +20,65 @@ function PasswordGenerator() {
       <div>
         <label>
           Długość hasła:
-          <input type="range" min="4" max="32" />
+          <input
+            type="range"
+            min="4"
+            max="32"
+            value={length}
+            onChange={(e) => {
+              setLength(parseInt(e.target.value));
+              console.log("Zmieniono długość:", e.target.value);
+            }}
+          />
         </label>
       </div>
       <div>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={includeSymbols}
+            onChange={() => {
+              setIncludeSymbols(!includeSymbols);
+              console.log(
+                "Przełączanie opcji „uwzględnij symbole”:",
+                !includeSymbols
+              );
+            }}
+          />
           Znaki specjalne (!@#$%)
         </label>
       </div>
 
       <div>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={includeNumbers}
+            onChange={() => {
+              setIncludeNumbers(!includeNumbers);
+              console.log(
+                "Przełączanie opcji „uwzględnij cyfry”:",
+                !includeNumbers
+              );
+            }}
+          />
           Numery (123456)
         </label>
       </div>
 
       <div>
         <label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={includeUppercase}
+            onChange={() => {
+              setIncludeUppercase(!includeUppercase);
+              console.log(
+                "Przełączanie opcji „uwzględnij cyfry”:",
+                !includeUppercase
+              );
+            }}
+          />
           Wielkie litery (ABC)
         </label>
       </div>
