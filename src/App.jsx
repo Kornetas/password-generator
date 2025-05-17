0872f04f5d84ef26ea1b5b046be3b51ed7d15f0c
Arek <arek93r@gmail.com>
2025-05-17 10:15:46 +0200

    init project with vite + clean boilerplate from vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Generator Haseł</h1>
+      {/* Tutaj później dodamy komponent */}
+    </div>
+  );
+}
+
+export default App;
